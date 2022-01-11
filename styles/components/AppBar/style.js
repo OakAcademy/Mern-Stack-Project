c4bcfa02ff2618c1ad3e98cc7c67916e4d3bca94
OakAcademy <57633416+OakAcademy@users.noreply.github.com>
2022-01-11 12:10:44 +0300

    Add files via upload

---
+++ styles/components/AppBar/style.js
@@ -0,0 +1,41 @@
+const styles = {
+  header: {
+    color: "#fff",
+    textAlign: "center",
+    fontSize: "3rem",
+    background:
+      "linear-gradient(90deg, rgba(246,83,39,1) 0%, rgba(147,27,252,.95) 50%, rgba(246,83,39,1) 100%)",
+    height: "5rem",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    zIndex: "1",
+  },
+  title: {
+    color: "white",
+    margin: "0",
+    marginRight: "25px",
+  },
+  avatar: {
+    color: "#f56a00",
+    backgroundColor: "#fde3cf",
+    marginRight: "6px",
+  },
+  userInfo: {
+    position: "absolute",
+    right: "25px",
+    top: "20px",
+    display: "flex",
+    alignItems: "center",
+  },
+  login: {
+    position: "absolute",
+    right: "25px",
+    top: "25px",
+  },
+  homeLink: {
+    display: "flex",
+    alignItems: "center",
+  },
+};
+export default styles;
---
+++ styles/components/Home/styles.js
@@ -0,0 +1,10 @@
+const styles = {
+    sider:{
+        background: '#f0f2f5'
+    },
+    content:{
+        margin:'2rem'
+    }
+}
+
+export default styles
