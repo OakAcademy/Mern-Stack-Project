9c4e414971fa06f3473a56de1d35cc8ef79049e0
OakAcademy <57633416+OakAcademy@users.noreply.github.com>
2022-07-18 16:14:53 +0300

    delete server

--- server/routes/users.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { signup, login } from "../controllers/users.js";
-
-const router = Router();
-
-router.post("/signup", signup);
-router.post("/login", login);
-
-export default router;
